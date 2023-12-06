5ae6d845720112eec414f615c5344786962db7ab
ulmo369 <a01366802@itesm.mx>
2023-12-06 12:44:04 -0600

    init controller

--- Robot15/src/main.cpp
+++ Robot15/src/main.cpp
@@ -8,38 +8,49 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]         [Port(s)]
+// Drivetrain           drivetrain      1, 10           
+// Controller1          controller                    
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
+
 #include "vex.h"
+#include "bits/stdc++.h" //Librerias esenciales de C++
 using namespace vex;
 competition Competition;
 
-// define your global instances of motors and other devices here
-
 /*---------------------------------------------------------------------------*/
-/*                          Pre-Autonomous Functions                         */
+/*                                  pre_auton                                */
 /*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
+/*              ESTA ES LA CONFIGURACIÓN INICIAL DEL AUTONOMO                */
+/*             Nosotros no creamos esta función, se crea solita              */
+/*                            NO SE DEBE BORRAR!!                            */
 /*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
 }
 
 /*---------------------------------------------------------------------------*/
+/*                               display_info                                */
 /*                                                                           */
-/*                              Autonomous Task                              */
+/*             Aquí solo imprimimos algo, ya sea en la consola               */
+/*                                  o en cerebro                             */
+/*   Es buena para imprimir valores, como grados, medidas, posiciones, etc   */
+/*---------------------------------------------------------------------------*/
+
+void display_info(){
+  std::cout << "Hola desde la consola" << std::endl;
+  Brain.Screen.print("Hola desde el cerebro");
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                autonomous                                 */
 /*                                                                           */
 /*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
+/*                            a VEX Competition.                             */
 /*                                                                           */
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
@@ -51,11 +62,10 @@ void autonomous(void) {
 }
 
 /*---------------------------------------------------------------------------*/
-/*                                                                           */
-/*                              User Control Task                            */
+/*                                usercontrol                                */
 /*                                                                           */
 /*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
+/*                            a VEX Competition.                             */
 /*                                                                           */
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
@@ -77,9 +87,7 @@ void usercontrol(void) {
   }
 }
 
-//
-// Main will set up the competition functions and callbacks.
-//
+
 int main() {
   // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
@@ -90,6 +98,7 @@ int main() {
 
   // Prevent main from exiting with an infinite loop.
   while (true) {
+    display_info(); //Es bueno imprimir para que se muestren varios valores mientras corre el robot
     wait(100, msec);
   }
 }
--- Robot15/src/robot-config.cpp
+++ Robot15/src/robot-config.cpp
@@ -1,15 +1,61 @@
 #include "vex.h"
-
+#include "constants.h"
 using namespace vex;
 
-// A global instance of brain used for printing to the V5 brain screen
+
+// Cerebelo
 brain Brain;
 
-/**
- * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- *
- * This should be called at the start of your int main function.
- */
+// Control (mando)
+controller Controller1 = controller(primary);
+
+// Chassis
+inertial DrivetrainInertial = inertial(PORT13);
+motor RightDriveA = motor(PORT4, ratio36_1, false);
+motor RightDriveB = motor(PORT3, ratio36_1, false);
+motor LeftDriveA = motor(PORT1, ratio36_1, true);
+motor LeftDriveB = motor(PORT2, ratio36_1, true);
+motor_group LeftDriveSmart = motor_group(LeftDriveA, LeftDriveB);
+motor_group RightDriveSmart = motor_group(RightDriveA, RightDriveB);
+/*smartdrive Drivetrain = smartdrive(LeftDriveSmart, RightDriveSmart, DrivetrainInertial, 
+  WHEEL_TRAVEL, TRACK_WIDTH, TRACK_BASE, mm, EXT_GEAR_RATIO);*/
+
+
+bool RemoteControlCodeEnabled = true;
+bool DrivetrainLNeedsToBeStopped_Controller1 = true;
+bool DrivetrainRNeedsToBeStopped_Controller1 = true;
+
 void vexcodeInit(void) {
   // Nothing to initialize
+}
+
+void chassis_control(){
+  int drivetrainLeftSideSpeed = (Controller1.Axis3.position() + (0.7*Controller1.Axis1.position()))/2.0;
+  int drivetrainRightSideSpeed = (Controller1.Axis3.position() - (0.7*Controller1.Axis1.position()))/2.0;
+  
+  if (drivetrainLeftSideSpeed < JOYSTICK_DEADBAND && drivetrainLeftSideSpeed > -JOYSTICK_DEADBAND) {
+    if (DrivetrainLNeedsToBeStopped_Controller1) {
+      LeftDriveSmart.stop();
+      DrivetrainLNeedsToBeStopped_Controller1 = false;
+    }
+  } else {
+    DrivetrainLNeedsToBeStopped_Controller1 = true;
+  }
+  if (drivetrainRightSideSpeed < JOYSTICK_DEADBAND && drivetrainRightSideSpeed > -JOYSTICK_DEADBAND) {
+    if (DrivetrainRNeedsToBeStopped_Controller1) {
+      RightDriveSmart.stop();
+      DrivetrainRNeedsToBeStopped_Controller1 = false;
+    }
+  } else {
+    DrivetrainRNeedsToBeStopped_Controller1 = true;
+  }
+  
+  if (DrivetrainLNeedsToBeStopped_Controller1) {
+    LeftDriveSmart.setVelocity(drivetrainLeftSideSpeed, percent);
+    LeftDriveSmart.spin(forward);
+  }
+  if (DrivetrainRNeedsToBeStopped_Controller1) {
+    RightDriveSmart.setVelocity(drivetrainRightSideSpeed, percent);
+    RightDriveSmart.spin(forward);
+  }
 }
