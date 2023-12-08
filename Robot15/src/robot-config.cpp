9478c8cf3b5bf298d545907025184ded7d1ae617
ulmo369 <a01366802@itesm.mx>
2023-12-08 00:56:53 -0600

    added alitas with indexers

--- Robot15/src/robot-config.cpp
+++ Robot15/src/robot-config.cpp
@@ -2,6 +2,8 @@
 #include "constants.h"
 using namespace vex;
 
+bool WingAreOpen = false;
+
 // Cerebelo
 brain Brain;
 
@@ -13,19 +15,38 @@ controller Controller1 = controller(primary);
 //Motor verde 36:1
 //Motor azul   6:1
 inertial DrivetrainInertial = inertial(PORT13);
-motor RightDriveA = motor(PORT4, ratio18_1, true);
-motor RightDriveB = motor(PORT3, ratio18_1, true);
-motor LeftDriveA = motor(PORT1, ratio18_1, false);
-motor LeftDriveB = motor(PORT2, ratio18_1, false);
+motor RightDriveA = motor(PORT4, ratio18_1, false);
+motor RightDriveB = motor(PORT3, ratio18_1, false);
+motor LeftDriveA = motor(PORT1, ratio18_1, true);
+motor LeftDriveB = motor(PORT2, ratio18_1, true);
 motor_group LeftDriveSmart = motor_group(LeftDriveA, LeftDriveB);
 motor_group RightDriveSmart = motor_group(RightDriveA, RightDriveB);
 smartdrive Drivetrain = smartdrive(LeftDriveSmart, RightDriveSmart, DrivetrainInertial, 
   WHEEL_TRAVEL, TRACK_WIDTH, TRACK_BASE, mm, EXT_GEAR_RATIO);
 
+// Wings 
+motor Wing = motor(PORT10, ratio18_1, true);
+pneumatics IndexerBoth = pneumatics(Brain.ThreeWirePort.A);
 
 bool RemoteControlCodeEnabled = true;
 bool DrivetrainLNeedsToBeStopped_Controller1 = true;
 bool DrivetrainRNeedsToBeStopped_Controller1 = true;
+
+void Wings_cb(){
+  //If the wings are open then we close them
+  if (!WingAreOpen) {
+    //Wing.spinToPosition(100, degrees, true);
+    IndexerBoth.set(true);
+    WingAreOpen = true;
+  }
+  //If the wings are close then we open them
+  else {
+    //Wing.spinToPosition(-100, degrees, true);
+    IndexerBoth.set(false);
+    WingAreOpen = false;
+  }
+}
+
 
 
 /*--------------------------------------------------------------------------*/
@@ -38,6 +59,9 @@ int rc_auto_loop_function_Controller1() {
   //Funciones de botones y sistemas
   //..............................
   //..............................
+
+  Controller1.ButtonB.pressed(Wings_cb);
+  Controller1.ButtonX.pressed(Wings_cb);
   while(true) {
     chassis_control();
   }
@@ -70,11 +94,12 @@ void vexcodeInit( void ) {
 /*           drivetrainLeftSideSpeed y de drivetrainRightSideSpeed          */
 /*--------------------------------------------------------------------------*/
 void chassis_control(){
+  //DESCOMENTAR ESTO PARA UNA VELOCIDAD MANEJABLE MEDIO LENTA MEDIO RAPIDA
   //int drivetrainLeftSideSpeed = (Controller1.Axis3.position() - (0.7*Controller1.Axis1.position()))/2.0;
   //int drivetrainRightSideSpeed = (Controller1.Axis3.position() + (0.7*Controller1.Axis1.position()))/2.0;
 
-  int drivetrainLeftSideSpeed = (Controller1.Axis3.position() - (Controller1.Axis1.position()));
-  int drivetrainRightSideSpeed = (Controller1.Axis3.position() + (Controller1.Axis1.position()));
+  int drivetrainLeftSideSpeed = (Controller1.Axis3.position() + (Controller1.Axis1.position()));
+  int drivetrainRightSideSpeed = (Controller1.Axis3.position() - (Controller1.Axis1.position()));
   
   if (drivetrainLeftSideSpeed < JOYSTICK_DEADBAND && drivetrainLeftSideSpeed > -JOYSTICK_DEADBAND) {
     if (DrivetrainLNeedsToBeStopped_Controller1) {
